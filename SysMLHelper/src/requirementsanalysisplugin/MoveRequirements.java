84717524f55af5d9855d0ae958a7c5c2db62742f
fchadburn <fraser.chadburn@mbsetraining.com>
2016-11-25 12:19:04 +0000

    Added #120 to 133 to make v2.0.29
    #120 25-NOV-2016: Enable TestDriver inheritance in the FullSim block creation dialog (F.J.Chadburn)
    #121 25-NOV-2016: Move unclaimed requirements ready for Gateway synch now copes with duplicate names (F.J.Chadburn)
    #122 25-NOV-2016: Scroll-bar added to Copy AD dialog to enable it to scale to large number of ADs (F.J.Chadburn)
    #123 25-NOV-2016: Improved Publish/Subscribe ports to clean up AutoRipple dependencies when doing copy/paste (F.J.Chadburn)
    #124 25-NOV-2016: Cleaned up unused code from PortCreator (F.J.Chadburn)
    #125 25-NOV-2016: AutoRipple used in UpdateTracedAttributePanel to keep check and FlowPort name updated (F.J.Chadburn)
    #126 25-NOV-2016: Fixes to CreateNewActorPanel to cope better when multiple blocks are in play (F.J.Chadburn)
    #127 25-NOV-2016: Improved usability of ViaPanel event creation by enabling default selection via tags (F.J.Chadburn)
    #128 25-NOV-2016: Improved usability/speed of Copy AD dialog by providing user choice to open diagrams (F.J.

--- SysMLHelper/src/requirementsanalysisplugin/MoveRequirements.java
+++ SysMLHelper/src/requirementsanalysisplugin/MoveRequirements.java
@@ -116,24 +116,33 @@ public class MoveRequirements {
 						int count = 0;
 						
 						for (IRPModelElement theReqt : theUnclaimedReqts) {
+							
 							// check if already element of same name
 							IRPModelElement alreadyExistingEl = thePackage.findNestedElement(theReqt.getName(), "Requirement");
 							
 							if (alreadyExistingEl != null){
-								Logger.writeLine("Error: Unable to move " + Logger.elementInfo( theReqt ) 
-										+ " as requirement of same name already exists under " 
-										+ Logger.elementInfo(thePackage) + ". Consider renaming it.");
-							} else {
-								Logger.writeLine("Moving " + Logger.elementInfo( theReqt ) + " from " 
-										+ Logger.elementInfo(theReqt.getOwner()) + " to " + Logger.elementInfo(thePackage) 
-										+ " and applying " + Logger.elementInfo(theStereotypeToApply));
-								theReqt.setOwner( thePackage );
-								theReqt.addStereotype(theStereotypeToApply.getName(), "Requirement");
-								count++;
-								theReqt.highLightElement();
 								
-								GeneralHelpers.applyStereotypeToDeriveReqtDependenciesOriginatingFrom( theReqt, theStereotypeToApply );
+								String uniqueName = GeneralHelpers.determineUniqueNameBasedOn( 
+										theReqt.getName(), "Requirement", thePackage );
+								
+								Logger.writeLine("Warning: Same name as " + Logger.elementInfo( theReqt ) 
+										+ " already exists under " + Logger.elementInfo(thePackage) + ", hence element was renamed to " + uniqueName );
+								
+								theReqt.setName( uniqueName );
+
 							}
+
+							Logger.writeLine("Moving " + Logger.elementInfo( theReqt ) + " from " 
+									+ Logger.elementInfo(theReqt.getOwner()) + " to " + Logger.elementInfo(thePackage) 
+									+ " and applying " + Logger.elementInfo(theStereotypeToApply));
+							
+							theReqt.setOwner( thePackage );
+							theReqt.addStereotype(theStereotypeToApply.getName(), "Requirement");
+							count++;
+							theReqt.highLightElement();
+
+							GeneralHelpers.applyStereotypeToDeriveReqtDependenciesOriginatingFrom( 
+									theReqt, theStereotypeToApply );
 						}
 						
 						Logger.writeLine("Finished (" + count + " requirements were moved out of " + theUnclaimedReqts.size() + ")");
@@ -153,7 +162,8 @@ public class MoveRequirements {
     Change history:
     #004 10-APR-2016: Re-factored projects into single workspace (F.J.Chadburn)
     #043 03-JUL-2016: Add Derive downstream reqt for CallOps, InterfaceItems and Event Actions (F.J.Chadburn)
-        
+    #121 25-NOV-2016: Move unclaimed requirements ready for Gateway synch now copes with duplicate names (F.J.Chadburn)
+    
     This file is part of SysMLHelperPlugin.
 
     SysMLHelperPlugin is free software: you can redistribute it and/or modify
