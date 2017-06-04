7e600163d5ef21f5e1ff630159bbe503dda244f0
fchadburn <fraser.chadburn@mbsetraining.com>
2017-06-04 22:00:40 +0100

    Added #190 to 202 to make v2.1.g (patch)

--- SysMLHelper/src/generalhelpers/StatechartHelpers.java
+++ SysMLHelper/src/generalhelpers/StatechartHelpers.java
@@ -14,22 +14,24 @@ public class StatechartHelpers {
 	static public IRPState getStateCalled(
 			String theName, 
 			IRPStatechart inTheDiagram, 
-			IRPModelElement ownedByEl){
+			IRPModelElement ownedByEl ){
 		
 		@SuppressWarnings("unchecked")
-		List<IRPModelElement> theElsInDiagram = inTheDiagram.getElementsInDiagram().toList();
+		List<IRPModelElement> theElsInDiagram = 
+			inTheDiagram.getElementsInDiagram().toList();
 		
 		IRPState theState = null;
 		
 		int count = 0;
 		
-		for (IRPModelElement theEl : theElsInDiagram) {
+		for( IRPModelElement theEl : theElsInDiagram ){
 			
-			if (theEl instanceof IRPState 
-					&& theEl.getName().equals(theName)
-					&& getOwningClassifierFor(theEl).equals(ownedByEl)){
+			if( theEl instanceof IRPState 
+					&& theEl.getName().equals( theName )
+					&& getOwningClassifierFor( theEl ).equals( ownedByEl ) ){
 				
-				Logger.writeLine("Found state called " + theEl.getName() + " owned by " + theEl.getOwner().getFullPathName());
+				Logger.writeLine( "Found state called " + theEl.getName() + 
+						" owned by " + theEl.getOwner().getFullPathName() );
 				
 				theState = (IRPState) theEl;
 				count++;
@@ -37,54 +39,64 @@ public class StatechartHelpers {
 		}
 		
 		if (count != 1){
-			Logger.writeLine("Warning in getStateCalled (" + count + ") states called " + theName + " were found");
+			Logger.writeLine( "Warning in getStateCalled (" + count + 
+					") states called " + theName + " were found" );
 		}
 		
 		return theState;
 	}
-
 	
 	private static IRPModelElement getOwningClassifierFor(
-			IRPModelElement theState){
+			IRPModelElement theState ){
 		
 		IRPModelElement theOwner = theState.getOwner();
 		
-		while (theOwner.getMetaClass().equals("State") || theOwner.getMetaClass().equals("Statechart")){
+		while( theOwner.getMetaClass().equals( "State" ) || 
+			   theOwner.getMetaClass().equals( "Statechart" ) ){
+			
 			theOwner = theOwner.getOwner();
 		}
 		
-		Logger.writeLine("The owner for " + Logger.elementInfo(theState) + " is " + Logger.elementInfo(theOwner));
+		Logger.writeLine( "The owner for " + Logger.elementInfo( theState ) + 
+				" is " + Logger.elementInfo( theOwner ) );
 			
 		return theOwner;
 	}	
 
 	static public IRPGraphElement findGraphEl(
 			IRPClassifier theClassifier, 
-			String withTheName) {
+			String withTheName ){
 		
 		IRPGraphElement theFoundGraphEl = null;
 		
 		@SuppressWarnings("unchecked")
 		List<IRPStatechartDiagram> theStatechartDiagrams = 
-				theClassifier.getStatechart().getNestedElementsByMetaClass("StatechartDiagram", 1).toList();
+				theClassifier.getStatechart().getNestedElementsByMetaClass(
+						"StatechartDiagram", 1 ).toList();
 		
 		for (IRPStatechartDiagram theStatechartDiagram : theStatechartDiagrams) {
 			
-			Logger.writeLine(theStatechartDiagram, "was found owned by " + Logger.elementInfo(theClassifier));
+			Logger.writeLine( theStatechartDiagram, "was found owned by " + 
+					Logger.elementInfo( theClassifier ) );
 			
 			@SuppressWarnings("unchecked")
-			List<IRPGraphElement> theGraphEls = theStatechartDiagram.getGraphicalElements().toList();
+			List<IRPGraphElement> theGraphEls = 
+				theStatechartDiagram.getGraphicalElements().toList();
 			
-			for (IRPGraphElement theGraphEl : theGraphEls) {
+			for( IRPGraphElement theGraphEl : theGraphEls ){
 				
 				IRPModelElement theEl = theGraphEl.getModelObject();
 				
-				if (theEl != null){
-					Logger.writeLine("Found " + theEl.getMetaClass() + " called " + theEl.getName());
+				if( theEl != null ){
+					Logger.writeLine( "Found " + theEl.getMetaClass() + 
+							" called " + theEl.getName() );
 					
-					if (theEl.getName().equals(withTheName)){
+					if( theEl.getName().equals( withTheName ) ){
 						
-						Logger.writeLine("Success, found GraphEl called " + withTheName + " in statechart for " + Logger.elementInfo(theClassifier));
+						Logger.writeLine( "Success, found GraphEl called " + 
+								withTheName + " in statechart for " + 
+								Logger.elementInfo( theClassifier ) );
+						
 						theFoundGraphEl = theGraphEl;
 						break;
 					}
@@ -94,5 +106,27 @@ public class StatechartHelpers {
 		
 		return theFoundGraphEl;
 	}
-
 }
+
+/**
+ * Copyright (C) 2016-2017  MBSE Training and Consulting Limited (www.executablembse.com)
+
+    Change history:
+    #198 05-JUN-2017: Support for adding MonitoringConditions transitions moved into shared StatechartHelpers (F.J.Chadburn)
+
+    This file is part of SysMLHelperPlugin.
+
+    SysMLHelperPlugin is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SysMLHelperPlugin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SysMLHelperPlugin.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
